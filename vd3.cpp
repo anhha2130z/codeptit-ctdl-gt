90e9d2a093b2e477d20e4f61379bbeda63f754e5
anhha2130z <146406534+anhha2130z@users.noreply.github.com>
2025-02-23 11:15:52 +0700

    Update vd3.cpp

--- vd3.cpp
+++ vd3.cpp
@@ -1,12 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	double s = 0;
+	long long s = 0;
 	int n; cin >> n;
-	if (n<=0)
-		return 0; 
-	for(int i=0;i<=n;i++)
-		s += 1.0*(2*i+1)/(2*i+2);
-	cout<<fixed<<setprecision(2)<<s<<endl;
+	if (n<0) return 0; 
+	for(int i=1;i<=n;i++)
+		s += pow(-1,i+1)*i;
+	cout<<s<<endl;
 	return 0;
 }
